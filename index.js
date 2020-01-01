38410c5a97807855259f8befdd1ca3b71678e6db
JuneJobs <j.jobs1028@gmail.com>
2020-01-02 06:31:49 +0900

    Update example 6

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.listen(_webPort, () => {
 */
 
 //example 4
-
+/*
 const bodyParser = require("body-parser");
 const express = require("express");
 const cors = require("cors");
@@ -76,3 +76,42 @@ router.get("/app.js", (req, res) => {
 app.listen(_webPort, () => {
     console.log(`web server running on ${_webPort}`);
 });
+*/
+
+//example 6
+/*
+const bodyParser = require("body-parser");
+const express = require("express");
+const cors = require("cors");
+
+const _webPort = 80;
+global.app = express();
+app.use(cors());
+global.router = express.Router();
+global.path = __dirname;
+
+app.use(bodyParser.json());
+app.use("/", router);
+
+router.get("/", (req, res) => {
+    console.log(path);
+    res.sendFile(`${path}/web-client/react.html`);
+});
+
+router.get("/app.js", (req, res) => {
+    res.sendFile(`${path}/web-client/app.js`);
+});
+
+//+
+router.post("/sum", (req, res) => {
+    let sum = req.body.param1 + req.body.param2;
+    let result = {
+        sum: sum
+    }
+    res.send(result);
+})
+
+app.listen(_webPort, () => {
+    console.log(`web server running on ${_webPort}`);
+});
+*/
